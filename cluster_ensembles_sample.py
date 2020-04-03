ac71a1854cd07a6722f4b2a20465ed64d73a0b8c
mahdiou <mmahdiou.diallo@gmail.com>
2020-04-03 15:49:08 +0200

    add skmeans execution, cleanup cluster_ensembles

--- cluster_ensembles_sample.py
+++ cluster_ensembles_sample.py
@@ -1,5 +1,3 @@
-import numpy as np
-import Cluster_Ensembles as CE
 from Cluster_Ensembles.Cluster_Ensembles import (
     build_hypergraph_adjacency,
     store_hypergraph_adjacency,
@@ -22,29 +20,25 @@ def cluster_ensembles(cluster_runs, verbose, N_clusters_max, method):
     return score, cluster_ensemble
 
 
-clustering_1 = [0, 1, 1, 2, 0, 2, 1, 0, 2, 1]
-clustering_2 = [0, 1, 2, 0, 0, 2, 2, 1, 2, 1]
-clustering_3 = [2, 0, 0, 2, 1, 1, 1, 0, 1, 2]
-cluster_runs = np.array([clustering_1, clustering_2, clustering_3])
-print("cluster_runs", cluster_runs)
+if __name__ == '__main__':
+    import numpy as np
+    clustering_1 = [0, 1, 1, 2, 0, 2, 1, 0, 2, 1]
+    clustering_2 = [0, 1, 2, 0, 0, 2, 2, 1, 2, 1]
+    clustering_3 = [2, 0, 0, 2, 1, 1, 1, 0, 1, 2]
+    cluster_runs = np.array([clustering_1, clustering_2, clustering_3])
+    print("cluster_runs", cluster_runs)
 
+    score, consensus_clustering_labels = cluster_ensembles(
+        cluster_runs, verbose=True, N_clusters_max=3, method=CSPA)
+    print('score', score)
+    print("consensus_clustering_labels", consensus_clustering_labels)
 
-score, consensus_clustering_labels = cluster_ensembles(
-    cluster_runs, verbose=True, N_clusters_max=3, method=CSPA)
-print('score', score)
-# consensus_clustering_labels = CE.cluster_ensembles(cluster_runs, verbose = True, N_clusters_max = 3)
-print("consensus_clustering_labels", consensus_clustering_labels)
+    score, consensus_clustering_labels = cluster_ensembles(
+        cluster_runs, verbose=True, N_clusters_max=3, method=MCLA)
+    print('score', score)
+    print("consensus_clustering_labels", consensus_clustering_labels)
 
-
-score, consensus_clustering_labels = cluster_ensembles(
-    cluster_runs, verbose=True, N_clusters_max=3, method=MCLA)
-print('score', score)
-# consensus_clustering_labels = CE.cluster_ensembles(cluster_runs, verbose = True, N_clusters_max = 3)
-print("consensus_clustering_labels", consensus_clustering_labels)
-
-
-score, consensus_clustering_labels = cluster_ensembles(
-    cluster_runs, verbose=True, N_clusters_max=3, method=HGPA)
-print('score', score)
-# consensus_clustering_labels = CE.cluster_ensembles(cluster_runs, verbose = True, N_clusters_max = 3)
-print("consensus_clustering_labels", consensus_clustering_labels)
+    score, consensus_clustering_labels = cluster_ensembles(
+        cluster_runs, verbose=True, N_clusters_max=3, method=HGPA)
+    print('score', score)
+    print("consensus_clustering_labels", consensus_clustering_labels)
